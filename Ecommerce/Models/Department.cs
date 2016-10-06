b2fbec39bcc34c4435e92149d5ae4893d522cc50
juapenagos <juanpenagoss@hotmail.com>
2016-10-05 22:52:19 -0500

    primer paso

---
+++ Ecommerce/Models/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Ecommerce.Models
+{
+    public class Department
+    {
+
+        [Key]
+        public int DepartmentId { get; set; } 
+
+        public String Name { get; set; }
+    }
+}
